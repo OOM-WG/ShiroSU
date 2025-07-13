908330de0aa70600d5dd6f560491b245a534e50c
梦璃酱 <yumeyuka@oom-wg.dev>
2025-07-13 11:59:31 +0800

    Refactor navigation and sidebar text for improved clarity and consistency
    Signed-off-by: 梦璃酱 <yumeyuka@oom-wg.dev>

--- docs/.vitepress/local/sidebar.ts
+++ docs/.vitepress/local/sidebar.ts
@@ -7,15 +7,15 @@ export const sidebar: DefaultTheme.Config["sidebar"] = {
             text: "快速了解",
             items: [
                 {
-                    text: "什么是 SSU",
+                    text: '<iconify-icon icon="f7:question-circle-fill" style="margin-right:0.25em;color:#58B2DC;"></iconify-icon>什么是 SSU',
                     link: "/base/overview",
                 },
                 {
-                    text: "更新日志",
+                    text: '<iconify-icon icon="f7:arrow-2-circlepath-circle-fill" style="margin-right:0.25em;color:#FF6B6B;"></iconify-icon>更新日志',
                     link: "/update/changelog",
                 },
                 {
-                    text: "历史版本",
+                    text: '<iconify-icon icon="fa6-solid:clock-rotate-left" style="margin-right:0.25em;color:#808080;"></iconify-icon>历史版本',
                     link: "/update/history",
                 },
             ],
@@ -24,25 +24,24 @@ export const sidebar: DefaultTheme.Config["sidebar"] = {
             text: "使用指南",
             items: [
                 {
-                    text: "安装指南",
+                    text: '<iconify-icon icon="f7:location-circle-fill" style="margin-right:0.25em;color:#4CD2DD;"></iconify-icon>安装指南',
                     link: "/base/install",
                 },
                 {
-                    text: "常见问题",
-                    link: "/base/faq",
-                },
+                    text: '<i"<iconify-icon icon=\"fa6-solid:wrench\" style=\"margin-right:0.25em;color:#A084DC;\"></iconify-icon>常见问题"                   link: "/base/faq",
+               },
             ],
         },
         {
             text: "面向开发者",
             items: [
                 {
-                    text: "模块开发",
-                    link: "/dev/module",
+                    text: "<iconify-icon icon=\"streamline-flex:module-puzzle-2-solid\" style=\"margin-right:0.25em;color:#6CB4EE;\"></iconify-icon>模块开发",
+                    link: "/dev/module"
                 },
                 {
-                    text: "模块 WebUI",
-                    link: "/dev/webui",
+                    text: "<iconify-icon icon=\"fa6-solid:globe\" style=\"margin-right:0.25em;color:#45B8AC;\"></iconify-icon>模块 WebUI",
+                    link: "/dev/webui"
                 },
             ],
         },
@@ -50,24 +49,24 @@ export const sidebar: DefaultTheme.Config["sidebar"] = {
             text: "关于 SSU",
             items: [
                 {
-                    text: "SSU 的起源",
-                    link: "/ssu/origin",
+                    text: "<iconify-icon icon=\"fa6-solid:seedling\" style=\"margin-right:0.25em;color:#66CC99;\"></iconify-icon>SSU 的起源",
+                    link: "/ssu/origin"
                 },
                 {
-                    text: "SSU 的原理",
-                    link: "/ssu/principle",
+                    text: "<iconify-icon icon=\"fa6-solid:book\" style=\"margin-right:0.25em;color:#FF9966;\"></iconify-icon>SSU 的原理",
+                    link: "/ssu/principle"
                 },
                 {
-                    text: "面对质疑",
-                    link: "/ssu/defense",
+                    text: "<iconify-icon icon=\"fa6-solid:comments\" style=\"margin-right:0.25em;color:#66CCCC;\"></iconify-icon>面对质疑",
+                    link: "/ssu/defense"
                 },
                 {
-                    text: "文档致谢",
-                    link: "/ssu/3q",
+                    text: "<iconify-icon icon=\"fa6-solid:handshake-angle\" style=\"margin-right:0.25em;color:#FF6666;\"></iconify-icon>文档致谢",
+                    link: "/ssu/3q"
                 },
                 {
-                    text: "关于团队",
-                    link: "/ssu/team",
+                    text: "<iconify-icon icon=\"fa6-solid:users\" style=\"margin-right:0.25em;color:#99CCFF;\"></iconify-icon>关于团队",
+                    link: "/ssu/team"
                 },
             ],
         },
@@ -75,16 +74,16 @@ export const sidebar: DefaultTheme.Config["sidebar"] = {
             text: "更多",
             items: [
                 {
-                    text: "许可证",
-                    link: "/more/license",
+                    text: "<iconify-icon icon=\"fa6-solid:id-card\" style=\"margin-right:0.25em;color:#58B2DC;\"></iconify-icon>许可证",
+                    link: "/more/license"
                 },
                 {
-                    text: "隐私政策",
-                    link: "/more/privacy",
+                    text: "<iconify-icon icon=\"fa6-solid:shield-halved\" style=\"margin-right:0.25em;color:#45B8AC;\"></iconify-icon>隐私政策",
+                    link: "/more/privacy"
                 },
                 {
-                    text: "翻译",
-                    link: "/more/translation",
+                    text: "<iconify-icon icon=\"fa6-solid:language\" style=\"margin-right:0.25em;color:#A084DC;\"></iconify-icon>翻译",
+                    link: "/more/translation"
                 },
             ],
         },
