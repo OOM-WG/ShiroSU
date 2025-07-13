2595cc4beb0901e61c345a3b74f4aecb1518ce77
梦璃酱 <yumeyuka@oom-wg.dev>
2025-07-13 11:43:07 +0800

    Refactor carousel and navigation text for improved readability
    Signed-off-by: 梦璃酱 <yumeyuka@oom-wg.dev>

--- docs/.vitepress/local/nav.ts
+++ docs/.vitepress/local/nav.ts
@@ -2,48 +2,48 @@ import type { DefaultTheme } from "vitepress";
 
 export const nav: DefaultTheme.Config["nav"] = [
     {
-        text: '<iconify-icon icon="fa6-solid:seedling" style="margin-right:0.25em;color:#66CC99;"></iconify-icon>关于 SSU',
+        text: "关于 SSU",
         items: [
             {
-                text: '<iconify-icon icon="fa6-solid:seedling" style="margin-right:0.25em;color:#66CC99;"></iconify-icon>起源',
+                text: "起源",
                 link: "/ssu/origin",
             },
             {
-                text: '<iconify-icon icon="fa6-solid:handshake-angle" style="margin-right:0.25em;color:#FF6666;"></iconify-icon>致谢',
+                text: "致谢",
                 link: "/ssu/3q",
             },
             {
-                text: '<iconify-icon icon="fa6-solid:users" style="margin-right:0.25em;color:#99CCFF;"></iconify-icon>团队',
+                text: "团队",
                 link: "/ssu/team",
             },
         ],
     },
     {
-        text: '<iconify-icon icon="f7:arrow-2-circlepath-circle-fill" style="margin-right:0.25em;color:#FF6B6B;"></iconify-icon>更新',
+        text: "更新",
         items: [
             {
-                text: '<iconify-icon icon="fa6-solid:clock-rotate-left" style="margin-right:0.25em;color:#808080;"></iconify-icon>历史版本',
+                text: "历史版本",
                 link: "/update/history",
             },
             {
-                text: '<iconify-icon icon="f7:arrow-2-circlepath-circle-fill" style="margin-right:0.25em;color:#FF6B6B;"></iconify-icon>更新日志',
+                text: "更新日志",
                 link: "/update/changelog",
             },
         ],
     },
     {
-        text: '<iconify-icon icon="fa6-solid:language" style="margin-right:0.25em;color:#A084DC;"></iconify-icon>多语言',
+        text: "多语言",
         items: [
             {
-                text: '<iconify-icon icon="fa6-solid:book-open-reader" style="margin-right:0.25em;color:#6CB4EE;"></iconify-icon>说明',
+                text: "说明",
                 link: "/more/translation",
             },
             {
-                text: '<iconify-icon icon="fa6-solid:globe" style="margin-right:0.25em;color:#45B8AC;"></iconify-icon>沉浸式翻译',
+                text: "沉浸式翻译",
                 link: "https://immersivetranslate.com/",
             },
             {
-                text: '<iconify-icon icon="fa6-solid:language" style="margin-right:0.25em;color:#A084DC;"></iconify-icon>DeepL',
+                text: "DeepL",
                 link: "https://www.deepl.com/ja/translator/",
             },
         ],
--- docs/.vitepress/theme/components/Carousel.vue
+++ docs/.vitepress/theme/components/Carousel.vue
@@ -1,50 +1,35 @@
 <template>
-    <div class="carousel-container">
-      <n-carousel 
-        autoplay 
-        :interval="4000"
-        :show-dots="false"
-        :show-arrow="false"
-        dot-type="line"
-        trigger="hover"
-      >
-        <img
-          class="carousel-img"
-          src="/assets/carousel/1.png"
-        >
-        <img
-          class="carousel-img"
-          src="/assets/carousel/2.png"
-        >
-        <img
-          class="carousel-img"
-          src="/assets/carousel/3.png"
-        >
-        <img
-          class="carousel-img"
-          src="/assets/carousel/4.png"
-        >
-      </n-carousel>
-    </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { NCarousel } from 'naive-ui'
-  </script>
-  
-  <style scoped>
-  .carousel-container {
-    width: 100%;
-    max-width: 800px;
-    margin: 0 auto 24px auto;
-    border-radius: 8px;
-    overflow: hidden;
-  }
-  
-  .carousel-img {
-    width: 100%;
-    height: 240px;
-    object-fit: cover;
-    display: block;
-  }
-  </style>
+  <div class="carousel-container">
+    <n-carousel
+      :interval="4000"
+      :show-arrow="false"
+      :show-dots="false"
+      autoplay
+      dot-type="line"
+      trigger="hover">
+      <img class="carousel-img" src="/assets/carousel/1.png" />
+      <img class="carousel-img" src="/assets/carousel/2.png" />
+    </n-carousel>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { NCarousel } from "naive-ui"
+</script>
+
+<style scoped>
+.carousel-container {
+  width: 100%;
+  max-width: 800px;
+  margin: 0 auto 24px auto;
+  border-radius: 8px;
+  overflow: hidden;
+}
+
+.carousel-img {
+  width: 100%;
+  height: 240px;
+  object-fit: cover;
+  display: block;
+}
+</style>
