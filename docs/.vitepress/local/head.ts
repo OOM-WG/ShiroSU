4e87d523afe69b8c803ca73c53eab83296e22abe
白彩恋 <shiro@oom-wg.dev>
2025-09-24 00:21:06 +0800

    Update

--- docs/.vitepress/local/head.ts
+++ docs/.vitepress/local/head.ts
@@ -53,23 +53,21 @@ export const head: HeadConfig[] = [
 	[
 		'script',
 		{},
-		`
-(function (w, d, s, q, i) {
-    w[q] = w[q] || [];
-    var f = d.getElementsByTagName(s)[0], j = d.createElement(s);
-    j.async = true;
-    j.id = 'beacon-aplus';
-    j.src = 'https://d.alicdn.com/alilog/mlog/aplus/' + i + '.js';
-    j.onerror = function() {
-      console.warn('警告：友盟统计加载失败，可能已被广告拦截插件拦截。');
-    };
-    f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'aplus_queue', '203467608');
-
+		`;(function (w, d, s, q, i) {
+	w[q] = w[q] || []
+	const f = d.getElementsByTagName(s)[0],
+		j = d.createElement(s)
+	j.async = true
+	j.id = 'beacon-aplus'
+	j.src = 'https://d.alicdn.com/alilog/mlog/aplus/' + i + '.js'
+	j.onerror = function () {
+		console.warn('警告：友盟统计加载失败，可能已被广告拦截插件拦截。')
+	}
+	f.parentNode.insertBefore(j, f)
+})(window, document, 'script', 'aplus_queue', '203467608')
 aplus_queue.push({
-    action: 'aplus.setMetaInfo',
-    arguments: ['appKey', '6849a077bc47b67d8382f404']
-});
-        `
+	action: 'aplus.setMetaInfo',
+	arguments: ['appKey', '6849a077bc47b67d8382f404']
+})`
 	]
 ]
