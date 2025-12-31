156dfd53ef4cd435104a55ca77418a84fda3bbf6
白彩恋 <shiro@oom-wg.dev>
2025-12-31 18:59:51 +0800

    rm -rf docs

--- docs/src/public/functions/assets/[[path]].js
+++
@@ -1,7 +0,0 @@
-export function onRequest(context) {
-	const {path} = context.params
-	return Response.redirect(
-		`https://ssu.noidx.sawahara.host/assets/${Array.isArray(path) ? path.join('/') : path || ''}`,
-		301
-	)
-}
--- docs/src/zh/ssu/principle.md
+++
@@ -1,7 +0,0 @@
----
-layout: doc
-title: ShiroSU 内核原理
-footer: false
----
-
-# **参见 [回忆溢出文章](https://oom-wg.dev/posts/ssu-ko-fund)**
